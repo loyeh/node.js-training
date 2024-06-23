8bc33e51e9ab52fb32ddc2322d437da978112bd8
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-06-23 20:58:03 +0330

    lesson 7

--- app.js
+++ app.js
@@ -3,15 +3,39 @@ const express = require("express");
 //express app
 const app = express();
 
+//register view rngine
+app.set("view engine", "ejs");
+
 //listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  //res.send("<p>home page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  //res.send("<p>about page</p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "about" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "create a new blog" });
+});
+
+//404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
 });
