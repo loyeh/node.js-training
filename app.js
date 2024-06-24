b6590adef93d8595d6465aef2b39f5ebcacc8629
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-06-24 12:00:25 +0330

    lesson 8

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 //express app
 const app = express();
@@ -6,8 +7,26 @@ const app = express();
 //register view rngine
 app.set("view engine", "ejs");
 
+// middleware & static files
+app.use(express.static("public"));
+
 //listen for requests
 app.listen(3000);
+
+app.use((req, res, next) => {
+  console.log("new request made:");
+  console.log("host: ", req.hostname);
+  console.log("path: ", req.path);
+  console.log("method: ", req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("in the next middleware");
+  next();
+});
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
